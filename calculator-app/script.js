3f3903d4e692ddd0b1a98d1cbdbb0618571e7852
Bhoomika R <bhoomikarangegowda@gmail.com>
2026-02-06 11:29:48 +0530

    Sample Web apps

---
+++ calculator-app/script.js
@@ -0,0 +1,17 @@
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function clearResult() {
+    document.getElementById("result").value = "";
+}
+
+function calculate() {
+    let result = document.getElementById("result").value;
+
+    try {
+        document.getElementById("result").value = eval(result);
+    } catch {
+        alert("Invalid Expression");
+    }
+}
